ce564ac4b52a18d8ee128743925839ee370094dc
yrq <yrq@yrqtekimac.local>
2015-01-13 13:47:50 +0800

    Initial Commit

---
+++ XiaoZhuShou/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XiaoZhuShou
+//
+//  Created by yrq on 15-1-13.
+//  Copyright (c) 2015年 yrq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ XiaoZhuShou/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XiaoZhuShou
+//
+//  Created by yrq on 15-1-13.
+//  Copyright (c) 2015年 yrq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
